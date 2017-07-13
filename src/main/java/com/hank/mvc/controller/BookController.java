72e4ed1df09634470b454b01251865a766f317cb
Hank Guo <hguo@walmart.com>
2017-07-13 17:11:57 +0800

    Test the customized validators.

--- src/main/java/com/hank/mvc/controller/BookController.java
+++ src/main/java/com/hank/mvc/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.hank.mvc.controller;
 
+import com.hank.dao.BookRepository;
+import com.hank.domain.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,48 +9,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hank.dao.BookRepository;
-import com.hank.domain.Book;
-
 @RestController
 @RequestMapping(value = "/books", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BookController {
 
-	@Autowired
-	private BookRepository bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
-	@RequestMapping
-	public Iterable<Book> books() {
-		return bookRepository.findAll(); 
-	}
+    @RequestMapping
+    public Iterable<Book> books() {
+        return bookRepository.findAll();
+    }
 
-	@RequestMapping(value = "/{isbn}")
-	public Book book(@PathVariable("isbn") String isbn) {
-		return bookRepository.findOne(isbn);
-	}
+    @RequestMapping(value = "/{isbn}")
+    public Book book(@PathVariable("isbn") String isbn) {
+        return bookRepository.findOne(isbn);
+    }
 
-	@RequestMapping(value = "/{isbn}", method = RequestMethod.DELETE)
-	public String deleteBook(@PathVariable("isbn") String isbn) {
-		try {
-			bookRepository.delete(isbn);
-			return String.format("Book [%s] successfully deleted", isbn);
-		} catch (Exception e) {
-			return String.format("A problem occurred when deleting Book [%s]",
-					e.getMessage());
-		}
-	}
+    @RequestMapping(value = "/{isbn}", method = RequestMethod.DELETE)
+    public String deleteBook(@PathVariable("isbn") String isbn) {
+        try {
+            bookRepository.delete(isbn);
+            return String.format("Book [%s] successfully deleted", isbn);
+        } catch (Exception e) {
+            return String.format("A problem occurred when deleting Book [%s]",
+                    e.getMessage());
+        }
+    }
 
-	@RequestMapping("/author/{author}")
-	public Iterable<Book> booksByAuthor(@PathVariable("author") String author) {
-		return bookRepository.findBooksByAuthor(author);
-	}
-
-	public BookRepository getBookRepository() {
-		return bookRepository;
-	}
-
-	public void setBookRepository(BookRepository bookRepository) {
-		this.bookRepository = bookRepository;
-	}
-
+    @RequestMapping("/author/{author}")
+    public Iterable<Book> booksByAuthor(@PathVariable("author") String author) {
+        return bookRepository.findBooksByAuthor(author);
+    }
 }
