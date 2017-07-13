b5cf02be137c5e8d223255af776871e8813be160
Hank Guo <hguo@walmart.com>
2017-07-13 17:20:41 +0800

    Test the customized validators.

--- src/main/java/com/hank/mvc/validator/HankValidatorAdapter.java
+++ src/main/java/com/hank/mvc/validator/HankValidatorAdapter.java
@@ -1,51 +1,49 @@
 package com.hank.mvc.validator;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.validation.constraints.NotNull;
-
 import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import javax.validation.constraints.NotNull;
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class HankValidatorAdapter extends AbstractValidationAdapter {
-	private static final Logger logger = Logger.getLogger(AbstractValidationAdapter.class);
-	private ObjectMapper mapper = new ObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    private static final Logger logger = Logger.getLogger(AbstractValidationAdapter.class);
 
-	public Map<String, Map<String, String>> validationConfig(Class<?> cls) {
-		Map<String, Map<String, String>> res = new HashMap<String, Map<String, String>>();
-		for (final java.lang.reflect.Field f : cls.getDeclaredFields()) {
-			try {
-				Annotation[] anos = f.getAnnotations();
-				Map<String, String> rr = new HashMap<String, String>();
-				if (anos != null && anos.length > 0)
-					for (Annotation a : anos) {
-						rr.putAll(parseAnnotation(a));
-						if (a.annotationType().isAssignableFrom(NotNull.class)) {
-							rr.put("required", "true");
-						} else {
-							rr.put("type", a.annotationType().getSimpleName());
-						}
-					}
-				res.put(f.getName(), rr);
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				e.printStackTrace();
-			}
-		}
-		return res;
-	}
-
-	@Override
-	public String parse(Class<?> cls) throws Exception {
-		return mapper.writeValueAsString(validationConfig(cls));
-	}
-
+    @Override
+    public JSONObject parse(Class<?> cls) throws JSONException {
+        JSONObject res = new JSONObject();
+        for (final java.lang.reflect.Field f : cls.getDeclaredFields()) {
+            try {
+                List<Annotation> annotations = Arrays.asList(f.getAnnotations())
+                        .stream()
+                        .filter(a -> a.annotationType().getName().contains("javax.validation.constraints"))
+                        .collect(Collectors.toList());
+                if (annotations != null && annotations.size() > 0) {
+                    JSONObject rr = null;
+                    for (Annotation a : annotations) {
+                        logger.info("parsing annotation " + a.annotationType().getName());
+                        if (rr == null) {
+                            rr = new JSONObject(parseAnnotation(a));
+                        }
+                        if (a.annotationType().isAssignableFrom(NotNull.class)) {
+                            rr.put("required", true);
+                        } else {
+                            rr.put("type", a.annotationType().getSimpleName());
+                        }
+                    }
+                    res.put(f.getName(), rr);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        logger.info(res.toString(4));
+        return res;
+    }
 }
