d1ad71ad81933c7005912a0d0e9e920c9726aec8
Hank Guo <hguo@walmart.com>
2017-07-14 14:22:33 +0800

    Add swagger docs.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,21 @@ INSERT INTO book(isbn, title, author, description) values
 ),(
 	'2222222', 
 	'The Spirit Rebellion (The Legend of Eli Monpress, #2)', 
+	'Leon Liu',
+	'Eli Monpress is brilliant. H'
+),(
+	'2222222',
+	'The Spirit Rebellion (The Legend of Eli Monpress, #2)',
+	'Jack Chen',
+	'Eli Monpress is brilliant. H'
+),(
+	'2222212222',
+	'The Spirit Rebellion (The Legend of Eli Monpress, #2)',
 	'Rachel Aaron',
+	'Eli Monpress is brilliant. H'
+),(
+	'22222212122',
+	'The Spirit Rebellion (The Legend of Eli Monpress, #2)',
+	'Hank Guo',
 	'Eli Monpress is brilliant. H'
 );
